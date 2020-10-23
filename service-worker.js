aab7ac134e0e996cfee6c22415e97eb8a6e8f089
milla-romankova <milla-romankova@gmail.com>
2020-10-23 18:52:16 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1b6ea50562cf8abf248f73f3d229fa90.js"
+  "./precache-manifest.81ebf5a29613a3352992400dfa61b058.js"
 );
 
 self.addEventListener('message', (event) => {
